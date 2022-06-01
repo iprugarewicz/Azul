398575902d91265d0781bcd800f7f1290cde50e2
mwysock2 <01168976@pw.edu.pl>
2022-06-01 13:35:19 +0200

    add menu

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -7,8 +7,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-       Game game=new Game(2);
-       game.letsplay();
+        Menu.start();
+       //Game game=new Game(2);
+       //game.letsplay();
         //Game game2 = ReadGameStatusFromFile.readGameStatus(ObjectsSerializer.deserializeGameStatus("pierwszy.ser"));
         //game2.letsplay();
     }
